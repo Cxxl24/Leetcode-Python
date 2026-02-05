0017afc0fe7e62b2bbb94cce70d7fc40a8cbda48
Ben van der Westhuizen <27992748@sun.ac.za>
2026-02-05 17:35:07 +0200

    02/05

--- twosum.py
+++ twosum.py
@@ -1,10 +1,11 @@
-from typing import List
+from typing import Dict, List
 
 class Solution:
     def twoSum(self, nums : List[int], target : int) -> List[int]:
-        seen = {}
-        for i, num in enumerate(nums):
-            complement = target - num
-            if complement in seen:
-                return [seen[complement], i]
-            seen[num] = i
+        hashmap: Dict[int, int] = {}
+
+        for i in range(len(nums)):
+            if (target - nums[i]) in hashmap.values():
+                return [hashmap[target - nums[i]], i]
+            else: 
+                hashmap[nums[i]] = i
